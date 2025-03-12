870ec913b2e60ca6b0e994015ca62f5f1b14e134
aditi1628 <aditibhargavaofficial@gmail.com>
2025-03-12 16:35:13 +0530

    Added task-4 footer section

---
+++ task-4 footer section/footer.js
@@ -0,0 +1,17 @@
+const backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
